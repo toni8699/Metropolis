c5ccdbf03cf01761f811174be7d865f64117b20c
Tony <34341990+toni8699@users.noreply.github.com>
2026-02-22 23:11:59 -0500

    p2

---
+++ project2/droptbl.sql
@@ -0,0 +1,12 @@
+-- Include your drop table DDL statements in this file.
+-- Make sure to terminate each statement with a semicolon (;)
+
+-- LEAVE this statement on. It is required to connect to your database.
+CONNECT TO COMP421;
+
+-- Remember to put the drop table ddls for the tables with foreign key references
+--    BEFORE the ddls to drop the parent tables (reverse of the creation order).
+
+-- This is only an example of how you add drop table ddls to this file.
+--   You may remove it.
+DROP TABLE MYTEST01;
