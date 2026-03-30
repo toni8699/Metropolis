faa535e9da5538dca60f4d629fa9d6f9c7c0d0d7
Tony <34341990+toni8699@users.noreply.github.com>
2026-03-30 00:19:45 -0400

    final

--- Project3/Metropolis/q3_relocate_employees_proc.sql
+++ Project3/Metropolis/q3_relocate_employees_proc.sql
@@ -1,55 +1,19 @@
-CONNECT TO COMP421@
+DECLARE v_rows INT DEFAULT 0;
 
-DROP PROCEDURE MarkRentedVehiclesAndReport@
+...
 
-CREATE PROCEDURE MarkRentedVehiclesAndReport (
-    IN p_raise_pct DECIMAL(6,4),
-    OUT p_vehicle_updates INT,
-    OUT p_employee_updates INT
-)
-LANGUAGE SQL
-BEGIN
-    DECLARE v_vin CHAR(17);
-    DECLARE v_done INT DEFAULT 0;
+UPDATE Vehicle
+SET status = 'Rented'
+WHERE vin = v_vin
+  AND status <> 'Rented';
 
-    DECLARE c_vins CURSOR FOR
-        SELECT DISTINCT a.vin
-        FROM Agreement a;
+GET DIAGNOSTICS v_rows = ROW_COUNT;
+SET p_vehicle_updates = p_vehicle_updates + v_rows;
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_done = 1;
+...
 
-    SET p_vehicle_updates = 0;
-    SET p_employee_updates = 0;
+UPDATE Employee
+SET salary = salary * (1 + p_raise_pct)
+WHERE eID IN (SELECT DISTINCT eID FROM Agreement);
 
-    IF p_raise_pct < 0 THEN
-        SET p_raise_pct = 0;
-    END IF;
-
-    OPEN c_vins;
-
-    vehicle_loop:
-    LOOP
-        SET v_done = 0;
-        FETCH c_vins INTO v_vin;
-
-        IF v_done = 1 THEN
-            LEAVE vehicle_loop;
-        END IF;
-
-        UPDATE Vehicle
-        SET status = 'Rented'
-        WHERE vin = v_vin
-          AND status <> 'Rented';
-
-        SET p_vehicle_updates = p_vehicle_updates + ROW_COUNT;
-    END LOOP;
-
-    CLOSE c_vins;
-
-    UPDATE Employee
-    SET salary = salary * (1 + p_raise_pct)
-    WHERE eID IN (SELECT DISTINCT eID FROM Agreement);
-
-    SET p_employee_updates = ROW_COUNT;
-END@
-
+GET DIAGNOSTICS p_employee_updates = ROW_COUNT;
